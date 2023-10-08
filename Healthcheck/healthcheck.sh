57160a78348429c26bfcb5c70189d6249f12ea71
Farshad Nick <88557305+farshadnick@users.noreply.github.com>
2023-10-08 15:37:06 +0330

    Update healthcheck.sh

--- Healthcheck/healthcheck.sh
+++ Healthcheck/healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the URL to check
-URL="http://local1host/"
+URL="http://localhost/"
 
 # Make an HTTP GET request and store the response
 RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
